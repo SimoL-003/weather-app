427acf20efcaf9a109bf46f0d25ee8cb2f9c320d
SimoL-003 <simoneleuzzi2003@gmail.com>
2026-02-23 10:29:10 +0100

    fix(Secondary Weather Info): add dynamic titles to

--- src/components/atoms/SecondaryWeatherInfo.jsx
+++ src/components/atoms/SecondaryWeatherInfo.jsx
@@ -1,7 +1,7 @@
-export default function SecondaryWeatherInfo({ data, temperature }) {
+export default function SecondaryWeatherInfo({ title, data, temperature }) {
   return (
     <div className="bg-neutral-800 px-5 py-8 rounded-2xl border border-neutral-600 flex flex-col justify-center gap-5">
-      <h4 className="font-bricolage text-neutral-200 text-2xl">Feels Like</h4>
+      <h4 className="font-bricolage text-neutral-200 text-2xl">{title}</h4>
       <p className="font-light text-4xl">
         {data}
         {temperature}
--- src/components/organisms/Main.jsx
+++ src/components/organisms/Main.jsx
@@ -160,21 +160,25 @@ export default function Main() {
             <div className="grid grid-cols-2 my-9 gap-4">
               {/* Apparent temperature */}
               <SecondaryWeatherInfo
+                title={"Feels Like"}
                 data={weatherData.current.apparent_temperature}
                 temperature={weatherData.current_units.apparent_temperature}
               />
               {/* Relative humidity */}
               <SecondaryWeatherInfo
+                title={"Humidity"}
                 data={weatherData.current.relative_humidity_2m}
                 temperature={weatherData.current_units.relative_humidity_2m}
               />
               {/* Wind speed */}
               <SecondaryWeatherInfo
+                title={"Wind Speed"}
                 data={weatherData.current.wind_speed_10m}
                 temperature={weatherData.current_units.wind_speed_10m}
               />
               {/* Precipitation */}
               <SecondaryWeatherInfo
+                title={"Precipitation"}
                 data={weatherData.current.precipitation}
                 temperature={weatherData.current_units.precipitation}
               />
